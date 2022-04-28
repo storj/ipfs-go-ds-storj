storj/ipfs-go-ds-storj

commit beb0caee5b8bc8232503424872181c2dbf1f1acb

Replace /blocks literal with blockstore.BlockPrefix (#47)

storj.go
@@ -11,6 +11,7 @@ import (
 
 	ds "github.com/ipfs/go-datastore"
 	dsq "github.com/ipfs/go-datastore/query"
+	bs "github.com/ipfs/go-ipfs-blockstore"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/spacemonkeygo/monkit/v3"
 	"github.com/zeebo/errs"
@@ -61,7 +62,7 @@ func NewDatastore(ctx context.Context, db *db.DB, conf Config) (*Datastore, erro
 	}
 
 	packs := pack.NewStore(project, conf.Bucket)
-	blocks := block.NewStore("/blocks", db, packs).
+	blocks := block.NewStore(bs.BlockPrefix.String(), db, packs).
 		WithPackInterval(conf.PackInterval).
 		WithPackSize(conf.MinPackSize, conf.MaxPackSize)
 
@@ -225,7 +226,7 @@ func (storj *Datastore) Query(ctx context.Context, q dsq.Query) (result dsq.Resu
 		}
 	}()
 
-	if strings.HasPrefix(q.Prefix, "/blocks") {
+	if strings.HasPrefix(q.Prefix, bs.BlockPrefix.String()) {
 		return storj.blocks.Query(ctx, q)
 	}
 
@@ -256,11 +257,7 @@ func (storj *Datastore) Close() error {
 }
 
 func isBlockKey(key ds.Key) bool {
-	ns := key.Namespaces()
-	if len(ns) < 1 {
-		return false
-	}
-	return ns[0] == "blocks"
+	return bs.BlockPrefix.IsAncestorOf(key)
 }
 
 func trimFirstNamespace(key ds.Key) ds.Key {

commit 7597d68bb342eebd8900369295948f4572051019

Fix trigger of packing job
Regressed with #47.

storj.go
@@ -257,7 +257,7 @@ func (storj *Datastore) Close() error {
 }
 
 func isBlockKey(key ds.Key) bool {
-	return bs.BlockPrefix.IsAncestorOf(key)
+	return bs.BlockPrefix == key || bs.BlockPrefix.IsAncestorOf(key)
 }
 
 func trimFirstNamespace(key ds.Key) ds.Key {

pack/pack_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	ds "github.com/ipfs/go-datastore"
+	bs "github.com/ipfs/go-ipfs-blockstore"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -55,7 +56,7 @@ func runPackTest(t *testing.T, initPackStatus func(ctx *testcontext.Context, sto
 		initPackStatus(ctx, storj)
 
 		// Sync starts the pack chore
-		err := storj.Sync(ctx, ds.NewKey(""))
+		err := storj.Sync(ctx, bs.BlockPrefix)
 		require.NoError(t, err)
 
 		storj.TriggerWaitPacker()